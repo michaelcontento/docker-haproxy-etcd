ba29157c0a7949b47cb2f5f0334ade11f521bcd4
Michael Contento <michaelcontento@gmail.com>
2014-10-19 15:50:14 +0200

    indent with 4 spaces in all files

--- etcdctl-initial-lookup.bash
+++ etcdctl-initial-lookup.bash
@@ -5,6 +5,6 @@ HAPROXY_BACKEND_SERVICE=$(/haproxy-config-get HAPROXY_BACKEND_SERVICE)
 NODES=$(etcdctl ls --recursive /services/$HAPROXY_BACKEND_SERVICE)
 
 for NODE_NAME in $NODES; do
-  NODE_VALUE=$(etcdctl get $NODE_NAME)
-  ETCD_WATCH_ACTION="set" ETCD_WATCH_KEY=$NODE_NAME ETCD_WATCH_VALUE=$NODE_VALUE /haproxy-update
+    NODE_VALUE=$(etcdctl get $NODE_NAME)
+    ETCD_WATCH_ACTION="set" ETCD_WATCH_KEY=$NODE_NAME ETCD_WATCH_VALUE=$NODE_VALUE /haproxy-update
 done
